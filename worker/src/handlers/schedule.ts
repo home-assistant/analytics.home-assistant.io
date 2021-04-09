3df2760769956f2cf2690ed2d1dfc0975a48e256
Ludeeus <ludeeus@ludeeus.dev>
2021-04-09 22:30:32 +0000

    Fix trigger build order

--- worker/src/handlers/schedule.ts
+++ worker/src/handlers/schedule.ts
@@ -73,15 +73,15 @@ async function processQueue(): Promise<void> {
 
     core_analytics[timestampString] = queue_data;
 
+    // Trigger Netlify build
+    await fetch(NETLIFY_BUILD_HOOK, { method: "POST" });
+
     await KV.put(
       `${KV_PREFIX_HISTORY}:${timestampString}`,
       JSON.stringify(queue_data)
     );
     await KV.put(KV_KEY_CORE_ANALYTICS, JSON.stringify(core_analytics));
     queue.data = createQueueData();
-
-    // Trigger Netlify build
-    await fetch(NETLIFY_BUILD_HOOK, { method: "POST" });
   }
 
   await KV.put(KV_KEY_QUEUE, JSON.stringify(queue));
