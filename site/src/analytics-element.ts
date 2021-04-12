70c14ed74dac68a6e24cd85898b5a201317552b1
Ludeeus <ludeeus@ludeeus.dev>
2021-04-12 09:56:29 +0000

    Show map on mobile

--- site/src/analytics-element.ts
+++ site/src/analytics-element.ts
@@ -93,7 +93,7 @@ export class AnalyticsElement extends LitElement {
       <analytics-map
         .lastDataEntry=${lastDataEntry}
         .isDarkMode=${this._isDarkMode}
-        .showMap=${!this._isMobile && this._currentPage === "installations"}
+        .showMap=${this._currentPage === "installations"}
       >
       </analytics-map>
     `;
