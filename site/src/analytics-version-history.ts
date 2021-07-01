f284a77279f17ba365f605898d21b4ef5cf66564
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-07-01 23:18:06 +0200

    Add version history graph

---
+++ site/src/analytics-version-history.ts
@@ -0,0 +1,121 @@
+import { Checkbox } from "@material/mwc-checkbox";
+import {
+  css,
+  customElement,
+  html,
+  internalProperty,
+  LitElement,
+  property,
+} from "lit-element";
+import { AnalyticsDataHistory } from "../../worker/src/data";
+import "./components/analytics-chart";
+
+@customElement("analytics-version-history")
+export class AnalyticsVersionHistory extends LitElement {
+  @property({ attribute: false }) public historyData?: AnalyticsDataHistory[];
+
+  @property({ type: Boolean }) public isMobile = false;
+
+  @property({ type: Boolean }) public isDarkMode = false;
+
+  @internalProperty() private _logScale = false;
+
+  render() {
+    if (this.historyData === undefined) {
+      return html``;
+    }
+
+    const allVersions: Set<string> = new Set();
+    this.historyData.forEach((entry) => {
+      if (entry.versions) {
+        Object.keys(entry.versions).forEach((version) => allVersions.add(version));
+      }
+    });
+
+    const versionsOrdered: string[] = Array.from(allVersions)
+      .sort((a, b) => {
+        const mainVersionCmp =
+          parseInt(a.split(".")[0]) - parseInt(b.split(".")[0]);
+        if (mainVersionCmp !== 0) {
+          return mainVersionCmp;
+        }
+        return parseInt(a.split(".")[1]) - parseInt(b.split(".")[1]);
+      });
+
+    const rows = this.historyData
+      .filter((data) => data.versions !== undefined)
+      .map((entry) => {
+        const result = [];
+        result.push(new Date(Number(entry.timestamp)));
+        versionsOrdered.forEach((version) => {
+          // @ts-ignore: undefined version entries already filtered
+          result.push(entry.versions[version] || 0);
+        });
+        return result
+      });
+
+    const columns = [{ label: "Date", type: "date" }];
+    versionsOrdered.forEach((version) => {
+      columns.push({ "label": version, type: "number" });
+    });
+
+    return html`
+      ${!this.isMobile
+        ? html`<mwc-formfield label="Logarithmic scale">
+            <mwc-checkbox @change=${this._toggleLogScale}></mwc-checkbox>
+          </mwc-formfield>`
+        : ""}
+      <analytics-chart
+        chartType="line"
+        .columns=${columns}
+        .rows=${rows}
+        .options=${{
+          title: `Version history`,
+          hAxis: {
+            title: "Date",
+            titleTextStyle: {
+              color: this.isDarkMode ? "#e1e1e1" : "#212121",
+            },
+          },
+          vAxis: {
+            title: "Active installations",
+            logScale: this._logScale,
+            titleTextStyle: {
+              color: this.isDarkMode ? "#e1e1e1" : "#212121",
+            },
+          },
+        }}
+        .isDarkMode=${this.isDarkMode}
+        .isMobile=${this.isMobile}
+      >
+      </analytics-chart>
+    `;
+  }
+
+  private _toggleLogScale(ev: CustomEvent) {
+    this._logScale = (ev.currentTarget as Checkbox).checked;
+  }
+
+  static styles = css`
+    :host {
+      display: block;
+      position: relative;
+    }
+
+    mwc-formfield {
+      position: absolute;
+      right: 16px;
+      z-index: 9;
+    }
+    mwc-checkbox {
+      --mdc-theme-secondary: var(--primary-color);
+      --mdc-checkbox-unchecked-color: var(--secondary-text-color);
+    }
+  `;
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "analytics-version-history": AnalyticsVersionHistory;
+  }
+}
