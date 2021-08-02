4d862cc7745c60bc5ea275e803418d77064f009a
Joakim Sørensen <joasoe@gmail.com>
2021-08-02 18:46:20 +0200

    Add energy analytics (#266)

--- worker/src/utils/validate.ts
+++ worker/src/utils/validate.ts
@@ -72,6 +72,7 @@ export const IncomingPayloadStruct = object({
   operating_system: optional(
     object({ board: string(), version: optional(nullable(string())) })
   ),
+  energy: optional(object({ configured: boolean() })),
   user_count: optional(number()),
   uuid: size(string(), 32, 32),
   version: size(string(), 7, 22),
