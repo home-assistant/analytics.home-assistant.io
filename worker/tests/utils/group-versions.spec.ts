df360e37ae27eea349324aa0146be443086505b8
ludeeus <joasoe@gmail.com>
2023-10-03 09:17:38 +0000

    simplify tests

--- worker/tests/utils/group-versions.spec.ts
+++ worker/tests/utils/group-versions.spec.ts
@@ -1,5 +1,19 @@
 import { groupVersions } from "../../src/utils/group-versions";
 
+const sampleData = {
+  "1970.1.0": 11,
+  "2021.4.0": 60,
+  "2021.4.1": 30,
+  "2021.4.2": 20,
+  "2021.5.0b0": 100,
+  "2021.5.0": 20,
+  "2021.5.5": 30,
+  "2021.6.0.dev20210517": 8,
+  "2021.6.0.dev20210506": 11,
+  "2021.6.0": 100,
+  "2021.7.0.dev20210601": 7,
+};
+
 describe("groupVersions", function () {
   afterEach(() => {
     (global as any).WORKER_ENV = undefined;
@@ -7,50 +21,20 @@ describe("groupVersions", function () {
 
   it("test dev", function () {
     (global as any).WORKER_ENV = "dev";
-    const data = {
-      "1970.1.0": 11,
-      "2021.4.0": 60,
-      "2021.4.1": 30,
-      "2021.4.2": 20,
-      "2021.5.0b0": 100,
-      "2021.5.0": 20,
-      "2021.5.5": 30,
-      "2021.6.0.dev20210517": 8,
-      "2021.6.0.dev20210506": 11,
-      "2021.6.0": 100,
-      "2021.7.0.dev20210601": 7,
-    };
-    const versions = {
+    expect(groupVersions(sampleData)).toStrictEqual({
       "1970.1": 11,
       "2021.4": 110,
       "2021.5": 150,
       "2021.6": 119,
-    };
-
-    expect(groupVersions(data)).toStrictEqual(versions);
+    });
   });
 
   it("test production", function () {
     (global as any).WORKER_ENV = "production";
-    const data = {
-      "1970.1.0": 11,
-      "2021.4.0": 60,
-      "2021.4.1": 30,
-      "2021.4.2": 20,
-      "2021.5.0b0": 100,
-      "2021.5.0": 20,
-      "2021.5.5": 30,
-      "2021.6.0.dev20210517": 8,
-      "2021.6.0.dev20210506": 11,
-      "2021.6.0": 100,
-      "2021.7.0.dev20210601": 7,
-    };
-    const versions = {
+    expect(groupVersions(sampleData)).toStrictEqual({
       "2021.4": 110,
       "2021.5": 150,
       "2021.6": 119,
-    };
-
-    expect(groupVersions(data)).toStrictEqual(versions);
+    });
   });
 });
